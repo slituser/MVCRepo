4be230f07fc7e82275057392d32daddc48cc9eaf
slituser <slituser@yahoo.com>
2015-06-04 12:49:58 +0530

    Initial

---
+++ GitProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitProject.Startup))]
+namespace GitProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
